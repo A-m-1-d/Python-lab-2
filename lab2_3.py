2d3def02eb9f39736a086fa9a391511a8efa965d
A-m-1-d <gorozhankin1580@gmail.com>
2025-05-30 20:52:19 +0300

    Update lab2_3.py

--- lab2_3.py
+++ lab2_3.py
@@ -1,12 +1,11 @@
-# lab2_task3.py
 # Демонстрация работы модуля math_operations
 
-import math_operations  # Импортируем наш модуль
+import math_operations
 
 def main():
     a = 10
     b = 5
-    c = 0  # Для проверки деления на ноль
+    c = 0
 
     print("Сумма (10 + 5):", math_operations.add(a, b))
     print("Разность (10 - 5):", math_operations.subtract(a, b))
